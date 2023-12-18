6bfce3253292e2e7e9e095cc9259c19302244669
finnq <qdafengzi@gmail.com>
2023-12-18 20:04:45 +0800

    update

--- app/src/main/java/com/example/mycamerax/MainActivity.kt
+++ app/src/main/java/com/example/mycamerax/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.Matrix
 import android.hardware.camera2.CameraCharacteristics
 import android.hardware.camera2.CameraMetadata
@@ -17,7 +18,6 @@ import android.provider.MediaStore
 import android.renderscript.RenderScript
 import android.util.Log
 import android.util.Range
-import android.util.Size
 import android.view.Display
 import android.view.LayoutInflater
 import android.view.MotionEvent
@@ -26,6 +26,7 @@ import android.view.ScaleGestureDetector
 import android.view.Surface
 import android.view.ViewGroup
 import android.widget.SeekBar
+import androidx.activity.compose.setContent
 import androidx.annotation.OptIn
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.camera2.interop.Camera2CameraControl
@@ -61,6 +62,54 @@ import androidx.camera.video.VideoCapture
 import androidx.camera.video.VideoRecordEvent
 import androidx.camera.view.LifecycleCameraController
 import androidx.camera.view.PreviewView
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.detectTransformGestures
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.gestures.rememberDraggableState
+import androidx.compose.foundation.gestures.rememberTransformableState
+import androidx.compose.foundation.gestures.rotateBy
+import androidx.compose.foundation.gestures.transformable
+import androidx.compose.foundation.gestures.zoomBy
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.input.pointer.pointerInteropFilter
+import androidx.compose.ui.input.pointer.positionChange
+import androidx.compose.ui.input.pointer.positionChanged
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.center
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.content.ContextCompat
 import androidx.core.util.Consumer
 import androidx.lifecycle.LifecycleOwner
@@ -78,8 +127,6 @@ import jp.co.cyberagent.android.gpuimage.filter.GPUImageFilterGroup
 import jp.co.cyberagent.android.gpuimage.filter.GPUImageGrayscaleFilter
 import jp.co.cyberagent.android.gpuimage.filter.GPUImageSharpenFilter
 import jp.co.cyberagent.android.gpuimage.filter.GPUImageSketchFilter
-import jp.co.cyberagent.android.gpuimage.filter.GPUImageToneCurveFilter
-import jp.co.cyberagent.android.gpuimage.filter.GPUImageToonFilter
 import jp.co.cyberagent.android.gpuimage.filter.GPUImageWhiteBalanceFilter
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -92,6 +139,7 @@ import java.util.concurrent.Executors
 import kotlin.math.abs
 import kotlin.math.max
 import kotlin.math.min
+import kotlin.math.roundToInt
 
 
 @ExperimentalCamera2Interop
@@ -200,6 +248,237 @@ class MainActivity : AppCompatActivity() {
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //initView()
+        setContent {
+            ImageEditorWithRectangularText()
+        }
+    }
+
+
+    @Composable
+    fun ImageEditorWithRectangularText() {
+        val pxValue = LocalDensity.current.run { 1.dp.toPx() }
+        val paddingValue = LocalDensity.current.run { 10.dp.toPx() }
+
+        val textContent = remember() {
+            mutableStateOf("哈哈哈哇哈哈哈")
+        }
+        val rotationAngle = remember() {
+            mutableStateOf(0f)
+        }
+
+        val minScale = 0.5f
+        val maxScale = 2f
+        var scale by remember { mutableStateOf(1f) }
+
+        val hide = remember {
+            mutableStateOf(false)
+        }
+
+        val imageBitmap = BitmapFactory.decodeResource(
+            this.resources,
+            R.mipmap.icon11
+        ).asImageBitmap()
+
+
+        val scope = rememberCoroutineScope()
+
+        // 0 缩放 1 旋转
+        var type = remember {
+            mutableStateOf(0)
+        }
+
+        //字体大小
+        val fontSize  = remember {
+            derivedStateOf {
+                scale*20
+            }
+        }
+
+        val canScale = remember {
+            derivedStateOf {
+                fontSize.value>8f && fontSize.value<30f
+            }
+        }
+
+
+
+        var center by remember {
+            mutableStateOf(IntOffset(0,0))
+        }
+
+        var isRotating by remember { mutableStateOf(false) }
+
+        val transformState = rememberTransformableState { zoomChange, panChange, rotationChange ->
+            XLogger.d("zoomChange:$zoomChange  rotationChange:${rotationChange}  panChange:${panChange.y}")
+            if (type.value == 0) {
+                scale += (zoomChange / 10) * 0.1f
+            } else {
+                rotationAngle.value = rotationAngle.value + (rotationChange) * 10f
+            }
+        }
+
+
+        var offsetX by remember { mutableStateOf(0f) }
+        var offsetY by remember { mutableStateOf(0f) }
+
+
+        var initialTouchPosition by remember { mutableStateOf(Offset.Zero) }
+        var initialAngle by remember { mutableStateOf(0f) }
+
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(1f)
+        ) {
+            Image(
+                bitmap = imageBitmap,
+                contentDescription = null,
+                modifier = Modifier.fillMaxSize()
+            )
+        }
+
+        if (!hide.value){
+            Box(
+                modifier = Modifier
+                    .wrapContentSize()
+                    .offset {
+                        IntOffset(offsetX.roundToInt(), offsetY.roundToInt())
+                    }
+                    .graphicsLayer {
+                        rotationZ = rotationAngle.value
+                        scaleX = scale
+                        scaleY = scale
+                        transformOrigin = TransformOrigin.Center
+
+
+                    }
+                    .transformable(state = transformState)
+                    .drawBehind {
+                        drawRect(
+                            color = Color(0xFF0099A1),
+                            style = Stroke(width = pxValue),
+                            topLeft = Offset(paddingValue, paddingValue),
+                            size = Size(
+                                this.size.width - 2 * paddingValue,
+                                this.size.height - 2 * paddingValue
+                            )
+                        )
+                    }
+                    .onSizeChanged {
+                        XLogger.d("拖动--------->size change ${it.center}")
+                        if (type.value==0){
+                            return@onSizeChanged
+                        }
+                        center = it.center
+                    }
+            ) {
+                Text(
+                    text = textContent.value,
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .padding(10.dp)
+                        .align(Alignment.Center)
+                        .pointerInput(Unit) {
+                            XLogger.d("拖动 offset---------->")
+                            detectDragGestures { change, dragAmount ->
+                                offsetX += dragAmount.x
+                                offsetY += dragAmount.y
+                            }
+                        }
+                    ,
+                    color =  Color.Black,
+                    fontSize = (fontSize.value).sp,
+
+                    )
+                Image(
+                    modifier = Modifier
+                        .align(Alignment.TopStart)
+                        .clickable(onClick = {
+                            hide.value = true
+                        })
+                    ,
+                    imageVector = ImageVector.vectorResource(id = R.drawable.ic_editor_delete),
+                    contentDescription = "delete"
+                )
+                Image(
+                    modifier = Modifier
+                        .align(Alignment.BottomStart)
+                        .pointerInput(Unit) {
+                            detectDragGestures(
+                                onDragStart = { offset ->
+                                    initialAngle = calculateAngle(offset.x, offset.y, center.x.toFloat(), center.y.toFloat())
+
+                                },
+                                onDrag = { change, _ ->
+                                    val rotationCurrentAngle = calculateAngle(
+                                        change.position.x,
+                                        change.position.y,
+                                        center.x.toFloat(),
+                                        center.y.toFloat()
+                                    )
+                                    val rotationDelta = rotationCurrentAngle - initialAngle
+                                    rotationAngle.value += rotationDelta
+                                },
+                                onDragEnd = {
+
+                                }
+                            )
+                        }
+                    ,
+                    imageVector = ImageVector.vectorResource(id = R.drawable.ic_editor_rotate),
+                    contentDescription = "rotate"
+                )
+
+
+                Image(modifier = Modifier
+                    .align(Alignment.BottomEnd)
+//                    .pointerInput(Unit) {
+//                        detectTransformGestures(
+//                            onGesture = { _, pan, zoom, rotation ->
+//                                type.value = 0
+//                                scale *= zoom
+//                                scale = scale.coerceIn(minScale, maxScale)
+//                            }
+//                        )
+//                     }
+                    .pointerInput(Unit) {
+
+                        detectTransformGestures(
+                            onGesture = { centroid: Offset, pan: Offset, zoom: Float, rotation: Float ->
+                                scope.launch {
+                                    type.value = 0
+                                    XLogger.d("zoom======${zoom}=====>y:${pan.y}  x:${pan.x}")
+                                    transformState.zoomBy(pan.x)
+                                }
+                            }
+                        )
+                    }
+                    ,
+                    imageVector = ImageVector.vectorResource(id = R.drawable.ic_editor_scale),
+                    contentDescription = "scale"
+                )
+            }
+        }
+    }
+
+
+    private fun calculateAngle(x: Float, y: Float,width:Float,height: Float): Float {
+        val centerX = (width ) / 2f
+        val centerY = (height) / 2f
+
+        val dx = x - centerX
+        val dy = y - centerY
+
+        return Math.toDegrees(Math.atan2(dy.toDouble(), dx.toDouble())).toFloat()
+    }
+
+    private val Offset.angle: Float
+        get() = Math.toDegrees(Math.atan2(y.toDouble(), x.toDouble())).toFloat()
+
+
+    fun initView(){
         setContentView(mBinding.root)
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
         cameraProviderFuture.addListener(
@@ -532,6 +811,7 @@ class MainActivity : AppCompatActivity() {
         }
         mBinding.sdScale.addOnChangeListener(OnChangeListener { slider, value, fromUser ->
             mCamera?.let { camera ->
+
                 val linearZoomFuture = camera.cameraControl.setLinearZoom(
                     value / 100f
                 )
@@ -539,6 +819,8 @@ class MainActivity : AppCompatActivity() {
                 linearZoomFuture.addListener({
                     XLogger.d("------>开始")
                 }, cameraParamsExecutor)
+
+                XLogger.d("缩放：${camera.cameraInfo.zoomState.value?.zoomRatio}")
             }
         })
 
@@ -872,7 +1154,7 @@ class MainActivity : AppCompatActivity() {
         val rotation = mBinding.gupImage.display.rotation
         val cameraSelector = CameraSelector.Builder().requireLensFacing(lensFacing).build()
         val previewBuilder = Preview.Builder()
-        previewBuilder.setTargetResolution(Size(width, width))
+        previewBuilder.setTargetResolution(android.util.Size(width, width))
 //        previewBuilder.setTargetRotation(Surface.ROTATION_0)
 //        previewBuilder.setTargetAspectRatio(screenAspectRatio)
         Camera2Interop.Extender(previewBuilder)
@@ -936,15 +1218,15 @@ class MainActivity : AppCompatActivity() {
         } else {
             //照片
             mImageCapture = ImageCapture.Builder()
-//                .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
-                .setCaptureMode(ImageCapture.CAPTURE_MODE_MAXIMIZE_QUALITY)
+                .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+//                .setCaptureMode(ImageCapture.CAPTURE_MODE_MAXIMIZE_QUALITY)
                 // 我们请求宽高比，但没有分辨率来匹配预览配置，但让
                 // CameraX 针对最适合我们用例的特定分辨率进行优化
                 // .setTargetAspectRatio(screenAspectRatio)
                 // 设置初始目标旋转，如果旋转发生变化，我们将不得不再次调用它
                 // 在此用例的生命周期内
 //                .setTargetRotation(rotation)
-                .setTargetResolution(Size(width, width))
+                .setTargetResolution(android.util.Size(width, width))
 //            .setCaptureOptionUnpacker { config, builder ->
 //                config.config.
 //            }
@@ -965,7 +1247,7 @@ class MainActivity : AppCompatActivity() {
 //            .setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_RGBA_8888)
             .setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_YUV_420_888)
             .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
-            .setTargetResolution(Size(width, width))
+            .setTargetResolution(android.util.Size(width, width))
             .build()
 
         val yuvUtils = YuvUtils()
@@ -1018,10 +1300,8 @@ class MainActivity : AppCompatActivity() {
                 cameraSelector,
 //                useCaseGroup,
 
-                previewCase,
-                mImageAnalysis,
-                mVideoCapture,
-                mImageCapture,
+//                previewCase,
+                mImageAnalysis, mVideoCapture
             )
 
             val camera2Control = Camera2CameraControl.from(mCamera!!.cameraControl)
@@ -1060,6 +1340,8 @@ class MainActivity : AppCompatActivity() {
                 zoomState.value?.let { zoomState ->
                     minZoom = zoomState.minZoomRatio
                     maxZoom = zoomState.maxZoomRatio
+
+                    XLogger.d("缩放：${minZoom}  ${maxZoom}")
                 }
                 exposureState = camera.cameraInfo.exposureState
                 supportExposureState = exposureState?.isExposureCompensationSupported ?: true
@@ -1423,12 +1705,12 @@ class MainActivity : AppCompatActivity() {
 
     private val displayListener = object : DisplayManager.DisplayListener {
         override fun onDisplayChanged(displayId: Int) {
-            if (mBinding.root.display.displayId == displayId) {
-                val rotation = mBinding.root.display.rotation
-//                imageAnalysis.targetRotation = rotation
-//                imageCapture.targetRotation = rotation
-                XLogger.d("displayListener屏幕的方向：$rotation")
-            }
+//            if (mBinding.root.display.displayId == displayId) {
+//                val rotation = mBinding.root.display.rotation
+////                imageAnalysis.targetRotation = rotation
+////                imageCapture.targetRotation = rotation
+//                XLogger.d("displayListener屏幕的方向：$rotation")
+//            }
         }
 
         override fun onDisplayAdded(displayId: Int) {
